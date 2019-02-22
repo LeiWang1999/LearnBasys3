7bf41bfc81753ce301a8ee0f6f4d18dedc2718dc
princeling <2230647190@qq.com>
2019-02-22 13:40:46 +0800

    update .gitignore

--- IP_CoreTest/IP_CoreTest.ip_user_files/sim_scripts/counter_mod_m_0/ies/run.f
+++
@@ -1,8 +0,0 @@
--makelib ies_lib/xil_defaultlib \
-  "../../../../IP_CoreTest.srcs/sources_1/ip/counter_mod_m_0/counter_mod_m.v" \
-  "../../../../IP_CoreTest.srcs/sources_1/ip/counter_mod_m_0/sim/counter_mod_m_0.v" \
--endlib
--makelib ies_lib/xil_defaultlib \
-  glbl.v
--endlib
-
--- IP_CoreTest/IP_CoreTest.ip_user_files/sim_scripts/counter_mod_m_0/questa/elaborate.do
+++
@@ -1,1 +0,0 @@
-vopt -64 +acc -l elaborate.log -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip -work xil_defaultlib xil_defaultlib.counter_mod_m_0 xil_defaultlib.glbl -o counter_mod_m_0_opt
